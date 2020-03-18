23ef54b52678521351494b8091c24751036294b3
George Hawkins <george-hawkins@users.noreply.github.com>
2020-03-18 17:06:38 +0100

    Added error handling for http call (and some light logging).

--- src/app/access-points.service.ts
+++ src/app/access-points.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,19 @@ export class AccessPointsService {
 
   getAccessPoints(): Observable<string[][]> {
     return this.http.get<string[][]>(this.accessPointsUrl)
+      .pipe(
+        tap(_ => console.log('Retrieved access points.')),
+        catchError(this.handleError<string[][]>('getAccessPoints', []))
+      )
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`)
+      console.error('Error details:', error);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
